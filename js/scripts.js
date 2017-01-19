8b06d7bb8c85f735ecf108d86219a7662e41352d
Matt Caswell <Matt Caswell>
2017-01-18 22:36:34 -0800

    add third spec to my practice leap

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
 });
 
 var leapYear = function(year) {
-  if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0))
+ {
     return true;
   } else {
   return false;
